302e14f802b59cf58edf463892093927b49b7155
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-03-04 23:56:23 +0900

    Add question code

--- question/src/main.rs
+++ question/src/main.rs
@@ -1,3 +1,16 @@
+fn judg(i: i32) -> Result<(), String> {
+    match i % 2 == 0 {
+        true => Ok(()),
+        false => Err("偶数ではありません".to_string()),
+    }
+}
+
+fn temp(i: i32) -> Result<(), String> {
+    judg(i)?;
+    println!("偶数です");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    temp(2);
 }
